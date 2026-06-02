90d6a95b4e0752970aa20a626efa0b1478782d17
Bernardo Braga <bebraga12@gmail.com>
2026-06-02 17:18:52 -0300

    initial commit

---
+++ src/main/java/com/finasys/FinasysApplication.java
@@ -0,0 +1,13 @@
+package com.finasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinasysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finasys/FinasysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
